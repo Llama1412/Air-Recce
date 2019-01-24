ca02d6bf5903193de79c409566673b26f7313d0b
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2019-01-24 09:07:31 +0000

    made first page

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Air Recce Trainer</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="">
+						<a href="">
+							<b>Current Score: </b> 
+							<p class="text-center" id="score">0</p>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<script>session_start()</script>
+    </head>
+    <?php
+        include planeindex.php;
+    ?>
+	<body>
+
+
+
+
+	</body>
+</html>
+
