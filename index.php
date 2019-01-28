fa75f205695f2088d144edc39bc7da3a3beff18b
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2019-01-28 12:18:12 +0000

    background color

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
             include "chooseplanes.php";
         ?>
         <link rel="shortcut icon" type="image/png" href="images/favicon.ico">
+        <link rel="stylesheet" href="css\style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
