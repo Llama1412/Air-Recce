78b27eb59d540186a385165b0d41f6a40cae8fda
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2019-01-28 12:03:58 +0000

    fixed a bug

--- scoring.php
+++ scoring.php
@@ -2,15 +2,21 @@
     session_start();
     if (!isset($_SESSION["score"])) {
         $_SESSION["score"] = 0;
-    }
+    };
     
     function enterAnswer($answer) {
         if ($_SESSION["correctanswer"] == $answer) {
             correctAnswer();
-        }
-    }
+        } else {
+            wrongAnswer();
+        };
+    };
     
     function correctAnswer() {
         $_SESSION["score"] = $_SESSION["score"] + 1;
-    }
+    };
+
+    function wrongAnswer() {
+        echo "<script>alert('Wrong answer!   Correct answer was: ".$_SESSION["correctanswer"]."')</script>";
+    };
     ?>
